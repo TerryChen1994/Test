d45c8ce7b8cd8cc5ae4cd064d65206b1bc2fc208
TerryChen <chocoterry@163.com>
2022-07-27 03:21:35 +0900

    m

--- src/main/java/CSVParser.java
+++ src/main/java/CSVParser.java
@@ -1,42 +1,49 @@
 import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
+import com.opencsv.exceptions.CsvValidationException;
 
 import java.io.*;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CSVParser {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws URISyntaxException, IOException, CsvValidationException {
 
         URL resource = CSVParser.class.getClassLoader().getResource("list.csv");
         File input_file = new File(resource.toURI());
         CSVReader reader = new CSVReader(new InputStreamReader(new FileInputStream(input_file),"Shift-JIS"));
 
         File output_file1 = new File("output/output1.csv");
-        CSVWriter writer1 = new CSVWriter(new FileWriter(output_file1));
+        Writer wri1 = new OutputStreamWriter(new FileOutputStream(output_file1), "Shift_JIS");
+        CSVWriter writer1 = new CSVWriter(wri1);
 
         File output_file2 = new File("output/output2.csv");
-        CSVWriter writer2 = new CSVWriter(new FileWriter(output_file2));
+        Writer wri2 = new OutputStreamWriter(new FileOutputStream(output_file2), "Shift_JIS");
+        CSVWriter writer2 = new CSVWriter(wri2);
 
         File output_file3 = new File("output/output3.csv");
-        CSVWriter writer3 = new CSVWriter(new FileWriter(output_file3));
+        Writer wri3 = new OutputStreamWriter(new FileOutputStream(output_file3), "Shift_JIS");
+        CSVWriter writer3 = new CSVWriter(wri3);
 
         File output_file4 = new File("output/output4.csv");
-        CSVWriter writer4 = new CSVWriter(new FileWriter(output_file4));
+        Writer wri4 = new OutputStreamWriter(new FileOutputStream(output_file4), "Shift_JIS");
+        CSVWriter writer4 = new CSVWriter(wri4);
 
-        File output_file5 = new File("output/output5.csv");
-        CSVWriter writer5 = new CSVWriter(new FileWriter(output_file5));
+        File output_filen = new File("output/output_other.csv");
+        Writer wrin = new OutputStreamWriter(new FileOutputStream(output_filen), "Shift_JIS");
+        CSVWriter writern = new CSVWriter(wrin);
 
-        File output_filen = new File("output/outputn.csv");
-        CSVWriter writern = new CSVWriter(new FileWriter(output_filen));
+        File output_file_no = new File("output/output_not0248.csv");
+        Writer wrinot = new OutputStreamWriter(new FileOutputStream(output_file_no), "Shift_JIS");
+        CSVWriter writernot = new CSVWriter(wrinot);
 
         String[] header = reader.readNext();
         writer1.writeNext(header);writer1.flush();
         writer2.writeNext(header);writer2.flush();
         writer3.writeNext(header);writer3.flush();
         writer4.writeNext(header);writer4.flush();
-        writer5.writeNext(header);writer5.flush();
         writern.writeNext(header);writern.flush();
 
         String[] preLine = new String[0];
@@ -44,27 +51,177 @@ public class CSVParser {
         List<String[]> list = new ArrayList<>();
 
         while ((currentLine = reader.readNext()) != null) {
+
+            String product = currentLine[10].replaceAll("\n","")
+                    .replaceAll("\"","")
+                    .replaceAll("カラー:","")
+                    .replaceAll("サイズ:","")
+                    .replaceAll("【{1}.*。{1}","");
+            if(currentLine[8].equals("ins0248")) {
+                if (product.contains("グレー")) {
+                    product = product.replaceAll("グレー", "灰");
+                } else if (product.contains("アイボリー")) {
+                    product = product.replaceAll("アイボリー", "米");
+                } else if (product.contains("ブラック")) {
+                    product = product.replaceAll("ブラック", "黒");
+                } else if (product.contains("グリーン")) {
+                    product = product.replaceAll("グリーン", "緑");
+                } else if (product.contains("ネイビー")) {
+                    product = product.replaceAll("ネイビー", "紺");
+                }
+            }
+            currentLine[10] = product;
+
+            // 第一行以后
             if(preLine.length > 0){
-                if(currentLine[0].equals(preLine[0])){
-                    list.add(currentLine);
-                    preLine = currentLine;
-                } else {
-                    switch (list.size()){
-                        case 1: writer1.writeAll(list); writer1.flush();break;
-                        case 2: writer2.writeAll(list); writer2.flush();break;
-                        case 3: writer3.writeAll(list); writer3.flush();break;
-                        case 4: writer4.writeAll(list); writer4.flush();break;
-                        case 5: writer5.writeAll(list); writer5.flush();break;
-                        default: writern.writeAll(list); writern.flush();
+                if (!currentLine[0].equals(preLine[0])) {
+                    if(is0248(list)) {
+                        int sum = sum(list);
+                        // 1单
+                        if (list.size() == 1) {
+                            if (sum == 1) {
+                                writer1.writeNext(list.get(0));
+                                writer1.flush();
+                            } else if (sum == 2) {
+                                list.get(0)[10] = list.get(0)[10] + "*2";
+                                writer1.writeNext(list.get(0));
+                                writer1.flush();
+                            } else if (sum == 3) {
+                                writer1.writeNext(list.get(0));
+                                writer1.flush();
+                                list.get(0)[10] = list.get(0)[10] + "*2";
+                                writer1.writeNext(list.get(0));
+                                writer1.flush();
+                            } else if (sum == 4) {
+                                list.get(0)[10] = list.get(0)[10] + "*2";
+                                writer1.writeNext(list.get(0));
+                                writer1.flush();
+                                writer1.writeNext(list.get(0));
+                                writer1.flush();
+                            } else if (sum >= 5) {
+                                writern.writeAll(list);
+                                writern.flush();
+                            } else {
+                                throw new RuntimeException();
+                            }
+
+                        }
+                        // 2单
+                        else if (list.size() == 2) {
+                            if (sum == 2) {
+                                list.get(0)[10] = list.get(0)[10] + list.get(1)[10];
+                                writer2.writeNext(list.get(0));
+                                writer2.flush();
+                            } else if (sum == 3) {
+                                for (String[] order : list) {
+                                    if (Integer.parseInt(order[9]) == 1) {
+                                        writer2.writeNext(order);
+                                        writer2.flush();
+                                    } else if (Integer.parseInt(order[9]) == 2) {
+                                        order[10] = order[10] + "*2";
+                                        writer2.writeNext(order);
+                                        writer2.flush();
+                                    }
+                                }
+                            } else if (sum == 4) {
+                                list.get(0)[10] = list.get(0)[10] + "*2";
+                                writer2.writeNext(list.get(0));
+                                writer2.flush();
+                                list.get(1)[10] = list.get(1)[10] + "*2";
+                                writer2.writeNext(list.get(1));
+                                writer2.flush();
+                            } else if (sum >= 5) {
+                                writern.writeAll(list);
+                                writern.flush();
+                            } else {
+                                throw new RuntimeException();
+                            }
+                        }
+                        // 3单
+                        else if (list.size() == 3) {
+                            if (sum == 3) {
+                                writer3.writeNext(list.get(0));
+                                writer3.flush();
+                                list.get(1)[10] = list.get(1)[10] + list.get(2)[10];
+                                writer3.writeNext(list.get(1));
+                                writer3.flush();
+                            } else if (sum == 4) {
+                                ArrayList<String[]> singleList = new ArrayList<>();
+                                for (String[] order : list) {
+                                    if (Integer.parseInt(order[9]) == 1) {
+                                        singleList.add(order);
+                                    } else if (Integer.parseInt(order[9]) == 2) {
+                                        order[10] = order[10] + "*2";
+                                        writer3.writeNext(order);
+                                        writer3.flush();
+                                    }
+                                }
+                                singleList.get(0)[10] = singleList.get(0)[10] + singleList.get(1)[10];
+                                writer3.writeNext(singleList.get(0));
+                                writer3.flush();
+                            } else if (sum >= 5) {
+                                writern.writeAll(list);
+                                writern.flush();
+                            } else {
+                                throw new RuntimeException();
+                            }
+                        }
+                        // 4单
+                        else if (list.size() == 4) {
+                            if (sum == 4) {
+                                list.get(0)[10] = list.get(0)[10] + list.get(1)[10];
+                                writer4.writeNext(list.get(0));
+                                writer4.flush();
+                                list.get(2)[10] = list.get(2)[10] + list.get(3)[10];
+                                writer4.writeNext(list.get(2));
+                                writer4.flush();
+                            } else if (sum >= 5) {
+                                writern.writeAll(list);
+                                writern.flush();
+                            } else {
+                                throw new RuntimeException();
+                            }
+                        }
+                        // 5单以上
+                        else if (list.size() >= 5) {
+                            writern.writeAll(list);
+                            writern.flush();
+                        } else {
+                            throw new RuntimeException();
+                        }
+                    }
+                    else{
+                        writernot.writeAll(list);
+                        writernot.flush();
                     }
                     list.clear();
-                    list.add(currentLine);
-                    preLine = currentLine;
                 }
-            } else{
+                list.add(currentLine);
+                preLine = currentLine;
+            }
+            //　第一行
+            else{
                 list.add(currentLine);
                 preLine = currentLine;
             }
         }
+    }
+
+    public static int sum(List<String[]> list){
+        int sum = 0;
+        for (String[] current : list) {
+            int i = Integer.parseInt(current[9]);
+            sum = sum + i;
+        }
+        return sum;
+    }
+
+    public static boolean is0248(List<String[]> list){
+        for (String[] current : list) {
+            if(!current[8].equals("ins0248")){
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/main/java/Temp.java
+++ src/main/java/Temp.java
@@ -1,15 +1,39 @@
+import java.io.*;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Temp {
-    public static void main(String[] args) {
-        String str = "東京都文京区小石川３−１３−９ セボン小石川113号";
+    public static void main(String[] args) throws URISyntaxException, IOException {
+//        URL resource = CSVParser.class.getClassLoader().getResource("test.txt");
+//        File input_file = new File(resource.toURI());
+//        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(input_file));
+//        String line = new String(bis.readAllBytes());
+//        String[] list = line.split(",");
+//
+//
+//        String product = list[10].replaceAll("\n","")
+//                .replaceAll("\"","")
+//                .replaceAll("カラー:","")
+//                .replaceAll("サイズ:","")
+//                .replaceAll("【{1}.*。{1}","");
+//        if(product.contains("グレー")){
+//            product = product.replaceAll("グレー","灰");
+//        } else if(product.contains("アイボリー")){
+//            product = product.replaceAll("アイボリー","米");
+//        } else if(product.contains("ブラック")){
+//            product = product.replaceAll("ブラック","黒");
+//        } else if(product.contains("グリーン")){
+//            product = product.replaceAll("グリーン","緑");
+//        } else if(product.contains("ネイビー")){
+//            product = product.replaceAll("ネイビー","紺");
+//        } else {
+//            throw new RuntimeException();
+//        }
 
-        Pattern p = Pattern.compile(".*[0-9１２３４５６７８９]+(丁目|-|ー|番地|番)+[0-9１２３４５６７８９]+");
-        Matcher m = p.matcher(str);
-        m.find();
-        System.out.println(m.group());
-        String sub = str.substring(m.group().length()).replaceAll("　"," ");
-        System.out.println(sub.trim());
+        String s = "miXL";
+        s = s + "*2";
+        System.out.println(s);
     }
 }
